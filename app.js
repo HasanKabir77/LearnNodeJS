8ac2af029f8929cf988e88ac875557b61fcc45f9
Your NameHasanKabir77 <you@example.comhasankabir0720@gmail.com>
2024-05-08 19:22:57 +0600

    Complete Node Tutorial 10 Video

---
+++ app.js
@@ -0,0 +1,11 @@
+module.exports ={ 
+    x : 20,
+    y : 10,
+    // z : this.x + this.y
+
+    myFunction : function ()
+    {
+        console.log('Hello World!');
+    }
+
+}
--- index.js
+++ index.js
@@ -2,4 +2,40 @@
 
 let a = 10, b = 50;
 let z = a + b;
-console.log(z);
+// console.log(z);
+
+//  if(z === 60)
+// {
+//     console.log("The Z Value is : a + b = ", z);
+// }
+
+// for (let i = 0; i <= a; i++) 
+// {
+//     console.log(i);
+// }
+let myArray = [1,2,3,4,5,6,7,8,9,10]
+// console.log(myArray);
+
+/*
+    // This code is from app.js file for import 
+    module.exports ={ 
+    x : 20,
+    y : 10,
+    // z : this.x + this.y
+}
+*/
+
+// import variable from another file
+let app = require('./app')
+
+// console.log(app.myFunction());
+
+// let result = myArray.filter((item)=>{
+    // console.log(item);
+//     return item >=5
+// })
+// console.log(result);
+// const fs = require('fs');
+
+// fs.writeFileSync('helloWorld.js', 'console.log("Hello World!")')
+
