2b617491f00898f935daf373cd33424b5c32326a
Your NameHasanKabir77 <you@example.comhasankabir0720@gmail.com>
2024-05-19 19:55:24 +0600

    Video 22-23

--- 01_Core_Node/HTML_and_JSON.js
+++ 01_Core_Node/HTML_and_JSON.js
@@ -3,9 +3,21 @@ const path = require('path');
 
 const app = express();
 
-const publicPath = path.join(__dirname, 'index')
+const publicPath = path.join(__dirname, 'HTML')
 // console.log(publicPath);
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+
+app.get('', (_, resp)=>{
+    resp.sendFile(`${publicPath}/index.html`)
+})
+
+app.get('/about', (_, resp)=>{
+    resp.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('*', (_, resp)=>{
+    resp.sendFile(`${publicPath}/404.html`)
+})
 
 app.listen(4500);
